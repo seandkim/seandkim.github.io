040d5badbcde3a88c6e812b8290c19fbba125771
Sean Kim <seandkim14@gmail.com>
2018-06-01 18:10:42 -0400

    implemented simple focus animation on focus change

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,13 +1,54 @@
 import PropTypes from 'prop-types';
 import React, { Component } from 'react';
+import $ from 'jquery';
 import { connect } from 'react-redux';
+
+import BackArrow from '../assets/icon/back-arrow.svg';
 import '../css/Logo.css';
+import { TimelineMax } from 'gsap';
 
 class Logo extends Component {
+  initFocusAnimation() {
+    const duration = 0.5;
+    const tl = new TimelineMax();
+    tl.add('start');
+    tl.to($('#Logo .logo-text'), duration, {
+      scaleX: 0,
+      rotation: 360,
+      transformOrigin: 'center',
+      opacity: 0,
+    }, 'start');
+    tl.from($('#Logo .back-arrow'), duration, {
+      rotation: -360,
+      scaleX: 0,
+      opacity: 0,
+    }, 'start');
+
+    tl.pause();
+    return tl;
+  }
+
+  componentDidMount() {
+    this.focusAnimation = this.initFocusAnimation();
+  }
+
+  componentDidUpdate(prevProps) {
+    if (prevProps.focused !== this.props.focused) {
+      if (this.props.focused) {
+        // Change logo to back arrow
+        this.focusAnimation.play();
+      } else {
+        // Change back arrow to logo
+        this.focusAnimation.reverse();
+      }
+    }
+  }
+
   render() {
     return (
       <div id="Logo">
-        SEAN D KIM
+        <div className="logo-text">SEAN D KIM</div>
+        <div className="back-arrow"><img src={BackArrow} /></div>
       </div>
     );
   }
